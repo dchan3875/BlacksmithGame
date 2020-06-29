72c3c978d925add501d2da07c939de2ed84fc4c5
Darren Chan <dchan3875@bths.edu>
2020-06-29 15:20:35 -0400

    Initial

---
+++ Blacksmith/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+    Vector3 characterScale;
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterScale = transform.localScale;
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetAxis("Horizontal") < 0)
+            characterScale.x = -1;
+        if (Input.GetAxis("Horizontal") > 0)
+            characterScale.x = 1;
+
+        transform.localScale = characterScale;
+            
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
